56e11d018a6ba740cf45563ff9df7efeaad4d2cc
Azat Seyrek <azat.seyrek@adesso.com.tr>
2024-04-29 22:56:18 +0300

    custom button varriants added

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,19 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div className="font-bold text-green-300">
-      <Button>Click me</Button>
+    <div className="flex max-w-[200px] flex-col space-y-4 p-4">
+      <Button>Default</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="primaryOutline">Primary Outline</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="secondaryOutline">Primary</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="dangerOutline">Danger Outline</Button>
+      <Button variant="super">Super</Button>
+      <Button variant="superOutline">super Outline</Button>
+      <Button variant="ghost">ghost</Button>
+      <Button variant="sidebar">sidebar</Button>
+      <Button variant="sidebarOutline">sidebar Outline</Button>
     </div>
   );
 }
