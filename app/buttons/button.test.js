7c4d6c5cf04e2c24bff777b5f702e587d6f6749b
Azat Seyrek <azat.seyrek@adesso.com.tr>
2024-05-13 14:33:03 +0300

    db connection completed

--- app/buttons/button.test.js
+++ app/buttons/button.test.js
@@ -3,33 +3,26 @@ import { render, screen } from '@testing-library/react';
 import ButtonsPage from './page';
 
 describe('ButtonsPage Component', () => {
-  test('renders all buttons correctly', () => {
-    render(<ButtonsPage />);
+  const buttonLabels = [
+    'Default',
+    'Primary',
+    'Primary Outline',
+    'Secondary',
+    'Secondary Outline',
+    'Danger',
+    'Danger Outline',
+    'Super',
+    'Super Outline',
+    'Ghost',
+    'Sidebar',
+    'Sidebar Outline',
+  ];
 
-    const defaultButton = screen.getByText('Default');
-    const primaryButton = screen.getByText('Primary');
-    const primaryOutlineButton = screen.getByText('Primary Outline');
-    const secondaryButton = screen.getByText('Secondary');
-    const secondaryOutlineButton = screen.getByText('Primary');
-    const dangerButton = screen.getByText('Danger');
-    const dangerOutlineButton = screen.getByText('Danger Outline');
-    const superButton = screen.getByText('Super');
-    const superOutlineButton = screen.getByText('Super Outline');
-    const ghostButton = screen.getByText('Ghost');
-    const sidebarButton = screen.getByText('Sidebar');
-    const sidebarOutlineButton = screen.getByText('Sidebar Outline');
-
-    expect(defaultButton).toBeInTheDocument();
-    expect(primaryButton).toBeInTheDocument();
-    expect(primaryOutlineButton).toBeInTheDocument();
-    expect(secondaryButton).toBeInTheDocument();
-    expect(secondaryOutlineButton).toBeInTheDocument();
-    expect(dangerButton).toBeInTheDocument();
-    expect(dangerOutlineButton).toBeInTheDocument();
-    expect(superButton).toBeInTheDocument();
-    expect(superOutlineButton).toBeInTheDocument();
-    expect(ghostButton).toBeInTheDocument();
-    expect(sidebarButton).toBeInTheDocument();
-    expect(sidebarOutlineButton).toBeInTheDocument();
+  buttonLabels.forEach((label) => {
+    test(`renders ${label} button correctly`, () => {
+      render(<ButtonsPage />);
+      const button = screen.getByText(label);
+      expect(button).toBeInTheDocument();
+    });
   });
 });
--- db/drizzle.ts
+++ db/drizzle.ts
@@ -3,20 +3,7 @@ import { drizzle } from 'drizzle-orm/neon-http';
 
 import * as schema from './schema';
 
-let dbUrl: string;
-
-switch (process.env.NODE_ENV) {
-  case 'development':
-    dbUrl = process.env.DATABASE_URL_DEV!;
-    break;
-  case 'production':
-    dbUrl = process.env.DATABASE_URL!;
-    break;
-  default:
-    dbUrl = process.env.DATABASE_URL_DEV!;
-}
-
-const sql = neon(dbUrl);
+const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle(sql, { schema });
 
 export default db;
